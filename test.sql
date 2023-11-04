cdee514ff50fd3e4a340a751e248e3e9b06da85d
Obikoya <obikoya16@gmail.com>
2023-11-04 10:32:34 +0100

    new commit

--- test.sql
+++ test.sql
@@ -1,3 +1,9 @@
 use little_lemon;
 
-select * from menus;
+SELECT * FROM orders;
+
+SELECT b.BookingID, b.EmployeeID as 'ServerID', b.TableNo, b.GuestFirstName, 
+        o.BillAmount
+FROM Bookings as b
+JOIN Orders as o
+ON b.BookingID = o.BookingID;
